b9015c44910f239f5e1200489c1a20e7cc2070ef
andrey <vk-andrei@mail.ru>
2022-07-02 15:51:02 +0300

    Les 4. first half

--- app/src/main/java/com/example/lesson_04_resources/MainActivity.java
+++ app/src/main/java/com/example/lesson_04_resources/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.lesson_04_resources;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 
+import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -34,6 +38,45 @@ public class MainActivity extends AppCompatActivity {
 
         AppCompatImageView image = findViewById(R.id.imageView);
         loadImageFromAsset(image, "logo.png");
+
+        inList();
+
+    }
+
+    private void inList() {
+        LinearLayout layoutList = findViewById(R.id.layoutList);
+        String [] versions = getResources().getStringArray(R.array.version_names);
+        // При помощи этого объекта будем надувать элементы, спрятанные в android_item.xml
+        // Сами элементы вставляются при помощи inflater. При помощи метода getLayoutInflater()
+        // получаем inflater, где вызываем метод inflate(). Этот метод создаёт пользовательский элемент на
+        // основе переданного макета, а также передаёт, куда мы будем добавлять новый пользовательский
+        // элемент. Булевым параметром будем определять, добавлять прямо сейчас или оставить добавление
+        // на потом.
+        LayoutInflater layoutInflater = getLayoutInflater();
+
+        for (int i = 0; i < versions.length; i++) {
+            String version = versions[i];
+            // Достаём элемент из android_item.xml
+           View item = layoutInflater.inflate(R.layout.android_item, layoutList, false);
+            // Находим в этом элементе TextView
+            TextView tv = item.findViewById(R.id.textAndroid);
+            tv.setText(version);
+            // Получить из ресурсов массив указателей на изображения
+            // при помощи метода obtainTypedArray() получаем идентификаторы изображений в массив TypedArray. Чтобы
+            // получить идентификатор самого изображения, используем getResourceId() на массиве
+            // TypedArray. TypedArray следует использовать, когда необходимо получить данные, связанные с
+            // пользовательским интерфейсом, в данном случае — с идентификаторами изображений. Если просто
+            // получить массив целых значений, то идентификаторы ресурсов ещё не будут готовы.
+            TypedArray imgs = getResources().obtainTypedArray(R.array.version_logos);
+            // Выбрать по индексу подходящее изображение
+            AppCompatImageView imgLogo = item.findViewById(R.id.imageAndroid);
+            imgLogo.setImageResource(imgs.getResourceId(i, -1));
+
+            layoutList.addView(item);
+
+        }
+
+
 
     }
 
